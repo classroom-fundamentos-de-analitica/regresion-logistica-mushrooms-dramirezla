8175a5a1c5dd3af1a84b9546badaafb027ea0ccd
dramirezla <53583408+dramirezla@users.noreply.github.com>
2023-06-20 18:22:00 -0500

    era 50
    ksjdfksd

--- preguntas.py
+++ preguntas.py
@@ -103,7 +103,7 @@ def pregunta_02():
     (X_train, X_test, y_train, y_test,) = train_test_split(
         X,
         y,
-        test_size=0.15,
+        test_size=50,
         random_state=123,
     )
 
